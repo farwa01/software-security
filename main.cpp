735e0c95965e0d1cbba7e70aa48cdacdf5faf899
farwa01 <115239712+farwa01@users.noreply.github.com>
2022-11-09 20:20:54 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,8 +26,6 @@ bool EmailCheck (string email);
 bool NumberCheck (int num);
 
 
-  // define any other function here.
-
 // Main Program
 int
 main ()
@@ -38,10 +36,8 @@ int intialbalance = 10000;
 cout <<"\t **************** WELCOME MR C++ EXPERT TO MY BANKING APPLICATION ******************* \n";
 cout << endl;
 cout <<"**********Please provide your credentials that are :: "<<endl
-
 <<" \t **** IP-Address" << endl
- <<"\t **** Email" <<
-endl
+ <<"\t **** Email" <<endl
     <<"\t **** ID " << endl << endl << endl;
 
     //string ipaddress;
@@ -75,19 +71,11 @@ cout <<"******************** WAIT !! We are Checking Your Credentials **********
 cout << endl << endl;
 cout <<" \t ***** What you want to Do ??? Choose the Action character *****"<<endl << endl;
 cout << " \t   W : Money Withdraw" << endl
-	<<"\t   D : Deposit"
-
-<<
-endl
+	<<"\t   D : Deposit"<<endl
  <<"\t   B : Balance" << endl
-	<<"\t   T : Transfer" <<
-
-endl
+	<<"\t   T : Transfer" <<endl
  <<"\t   C : Change Details" << endl
-
-<<"\t   Q : Quit or Exit"
- <<
-endl << endl;
+<<"\t   Q : Quit or Exit"<<endl << endl;
 string input;
 cin >> input;
 bool t = true;
@@ -143,145 +131,35 @@ t = false;
 
 cout << endl;
 
-
-
-
-
-
-
-
-cout <<
-
-
-
-
-
-
-
-"Select correct value for Example W for Withdraw .... Try Again"
-
-
-
-
-
-
-
-<<endl << endl;
-
-
-
-
-
-
-
-
+cout <<"Select correct value for Example W for Withdraw .... Try Again"<<endl << endl;
 cout << "   W : Money Withdraw" << endl
-		<<"   D : Deposit"
- <<
-endl
+		<<"  D : Deposit"<<endl
  <<"   B : Balance" << endl
 		<<"   T : Transfer" <<
-
 endl
  <<"   T : Transfer" << endl
-		<<"   C : Change Details"
+ <<"   C : Change Details"
  <<endl
- <<"   Q : Quit or Exit"
-		<<endl << endl;
-
-
-
-
-
-
-
+ <<" Q : Quit or Exit"<<endl << endl;
 
 cin >> input;
 
-
-
-
-
-
-
+}
 
 }
 
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
 
   else				// if checkresult is false
     {
 
-
-
-
-
-
-
-
 cout <<
-
-
 
 "\t \t **** check result failed, Please try again ****";
 
-
-
-
-
-
-
-
 }
 
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* Function Definition */
 
@@ -289,14 +167,6 @@ cout <<
 
 bool Validitycheck (char ip[16], string email, int num)
 {
-
-
-
-
-
-
-
-
     /* Here you need to perform three checks
        1. Call the Validate IP address Function -- Lab 4G.
        2. Call the Validate Email Address Function -- Lab 5A.
@@ -306,135 +176,29 @@ bool Validitycheck (char ip[16], string email, int num)
     // Example:
     bool xx;
 
-
-
-
-
-
-
-
 bool icheck = validateIP (ip);
-
-
-
-
-
-
-
 
 bool emailcheck = EmailCheck (email);
 
-
-
-
-
-
-
-
 bool numcheck = NumberCheck (num);
-
-
-
-
-
-
-
-
 
 if (icheck && emailcheck && numcheck)
 
-
-
-
-
-
-
-
     {
-
-
-
-
-
-
-
 
 cout << " \t \t Check Successful -------------- " << endl;	// you can return boolean true value to main()
       xx = true;
 
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
   else
-
-
-
-
-
-
-
-
     {
-
-
-
-
-
-
-
 
 cout << "\t \t Check Unsuccessful XXXXXXXXXXX" << endl;	//you can return boolean false value to main()
       xx = false;
-
-
-
-
-
-
-
-
 }
 
-
-
-
-
-
-
-
 cout << endl << endl;
-
-
-
-
-
-
-
-
 cout << " \t \t CHECK Completed ........" << endl;
-
-
-
-
-
-
-
-
-
-
 return xx;	// you should replace this to value your returning depending upon the credentials
 }
 
@@ -442,11 +206,6 @@ return xx;	// you should replace this to value your returning depending upon the
 void
 DepositMoney (int initialbalance)
 {
-
-
-
-
-
 
 int deposit;
 cout << "How much you want to deposit" << endl;
@@ -468,108 +227,29 @@ void
 withdrawMoney ()
 {
 
-
-
-
-
-
-
-
 // Get how much user want to withdraw and display balance after minus
 }
-
-
-
-
-
-
-
-
   // Function for Display Balance
 void
 displayBalance ()
 {
-
-
-
-
-
-
-
-
     // display balance after adding
 }
-
-
-
-
-
-
-
-
   //function for Transfer Money
 void
 Transfer (int initialbalance)
 {
-
-
-
-
-
-
 cout << "Transfer Balance";
-
-
-
-
-
 
 int transfer;
 
-
-
-
-
-
 cout << "How much to transfer" << endl;
-
-
-
-
-
-
 cin >> transfer;
-
-
-
-
-
-
 initialbalance = initialbalance - transfer;
-
-
-
-
-
-
 cout << "your remains are:<< initialbalance";
-
-
-
-
-
-
-
 
 // Get how much user want to transfer and display balance after transfer
 }
-
-
-
-
-
-
-
 
   // Function for change Details
 
@@ -583,58 +263,21 @@ ChangeDetails ()
 bool validateIP (char str[])
 {
 
-
-
-
-
-
-
 int iparray[4];	// this will hold values when "." is removed from the string e.g 192.168.1.1 ====> 192 168 1 1
   int i = 0;
-
-
-
-
-
-
 
     // The code to split the string using "." operator
   char *token = strtok (str, ".");
 
-
-
-
-
-
 while (token != NULL)
 
-
-
-
-
-
     {
-
-
-
-
-
 
 int num = stoi (token);	// this line convert string to integer. Example handling ip address 192.168.1.1
       iparray[i] = num;		// this line puts num to iparray for example iparray[0]=192, iparray[1]=168, iparray[2]=1, iparray[3]=1,
       token = strtok (NULL, ".");
 
-
-
-
-
-
 i++;
-
-
-
-
-
 
 }
 int getArrayLength = sizeof (iparray) / sizeof (int);	//sizeof(iparray) is library function returns size in bytes when need to find number of elements
@@ -642,105 +285,30 @@ int getArrayLength = sizeof (iparray) / sizeof (int);	//sizeof(iparray) is libra
   // if the token size is not equal to four
   if (getArrayLength - 1 != 3)
 
-
-
-
-
-
     {
-
-
-
-
-
 
 return false;
 
-
-
-
-
-
 }
-
-
-
-
-
-
-
     // validate each field of the IP-address
     for (int j = 0; j <= getArrayLength - 1; j++)
 
-
-
-
-
-
     {
-
-
-
-
-
 
 	// check number if greater >0 and less than 255
 	if (iparray[j] > 255 || iparray[j] < 0)
 
-
-
-
-
-
 	{
-
-
-
-
-
 
 return false;
 
-
-
-
-
-
 }
 
-
-
-
-
-
 }
-
-
-
-
-
 
 return true;
 
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
   /* This function only converts array of characters to string Optional only used for testing */
 
 string convertToString (char *a, int size)
@@ -748,44 +316,23 @@ string convertToString (char *a, int size)
 
 int i;
 
-
-
-
-
-
 string s = "";
-
-
-
-
-
 
 for (i = 0; i < size; i++)
 
     {
 s = s + a[i];
 }
-
-
-
 return s;
-
-
-
 }
 
 bool EmailCheck (string email)
 {
-
-
     // your code for number check return true or false value. Lab 5A.
 
 return true;
 
-
 }
-
-
 
 bool NumberCheck (int num)
 {
